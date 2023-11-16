1f0e602ea5cabdbc2bc317191ea6a7028e7efee8
ToastFish <15342907459@163.com>
2023-11-16 18:07:51 +0800

    添加JWT拦截器

--- give-it-to-you-backend/src/main/java/com/purehub/controller/user/UserController.java
+++ give-it-to-you-backend/src/main/java/com/purehub/controller/user/UserController.java
@@ -1,15 +1,17 @@
 package com.purehub.controller.user;
 
+import com.purehub.entity.Result;
 import com.purehub.entity.User;
 import com.purehub.service.user.UserService;
+import com.purehub.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class UserController {
 
   @Autowired
@@ -17,8 +19,16 @@ public class UserController {
 
   @ResponseBody
   @GetMapping("/login")
-  public User login() {
-    return userService.login("admin");
+  public Result login(@RequestParam String userName) {
+    User user = userService.login(userName); //从数据库获取到的user
+    //当查询到的用户不为空，创建JWT,并发送给前端
+    if(user!=null){
+      Map<String, Object> claims = new HashMap<>();
+      claims.put("id",user.getUserId());
+      String jwt = JwtUtils.generateJwt(claims);
+      return Result.success(jwt);
+    }
+    return Result.error("未查找到用户");
   }
 
   @ResponseBody
